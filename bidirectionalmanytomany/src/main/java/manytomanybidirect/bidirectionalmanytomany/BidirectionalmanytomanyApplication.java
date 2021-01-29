0d12e19787da00cca3a7d123a92c192a23d0d912
CindoddCindy <cindodcindy@gmail.com>
2021-01-29 07:49:18 +0700

    tambah folder

---
+++ bidirectionalmanytomany/src/main/java/manytomanybidirect/bidirectionalmanytomany/BidirectionalmanytomanyApplication.java
@@ -0,0 +1,13 @@
+package manytomanybidirect.bidirectionalmanytomany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidirectionalmanytomanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BidirectionalmanytomanyApplication.class, args);
+	}
+
+}
---
+++ bidirectionalmanytomany/src/test/java/manytomanybidirect/bidirectionalmanytomany/BidirectionalmanytomanyApplicationTests.java
@@ -0,0 +1,13 @@
+package manytomanybidirect.bidirectionalmanytomany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidirectionalmanytomanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
